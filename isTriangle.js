3dcc467a1bcc757dc095a8307c75105d11513c77
harshitadatra <95231941+Gautam2305@users.noreply.github.com>
2021-12-08 15:50:13 +0530

    update: isTriangel.js

---
+++ isTriangle.js
@@ -0,0 +1,23 @@
+var inputs= document.querySelectorAll(".angle-input");
+var isTriangleBtn =document.querySelector("#is-triangle-btn");
+var outputEl= document.querySelector("#output");
+
+function calculateSumOfAngles(angle1, angle2, angle3){
+    var sumOfAngles = angle1 + angle2 + angle3 
+    return sumOfAngles;
+}
+function isTriangle(){
+    var sumOfAngles = calculateSumOfAngles(Number(inputs[0].value), Number(inputs[1].value),Number(inputs[2].value))
+    
+    if (sumOfAngles === 180){
+        
+        outputEl.innerText="Yay!The angles form a triangle";
+    }else{
+        
+        outputEl.innerText="Oops! The angles don't form a triangle";
+    }
+}
+
+
+isTriangleBtn.addEventListener("click", isTriangle);
+

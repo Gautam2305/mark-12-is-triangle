2fdffae51c89b21f3b1110db45bea5f666de1fa7
harshitadatra <95231941+Gautam2305@users.noreply.github.com>
2021-12-08 20:00:43 +0530

    created: quiz.js | correction: quiz.html

---
+++ quiz.js
@@ -0,0 +1,20 @@
+var quizForm = document.querySelector(".quiz-form");
+var submitAnswerBtn = document.querySelector("#submit-answer-btn");
+var outputEl = document.querySelector("#output");
+
+var correctAnswers =["90°", "right-angled"]
+
+function calculateScore(){
+    let score = 0;
+    let index = 0;
+    var formResults = new FormData(quizForm);
+    for ( let value of formResults.values()){
+        if (value === correctAnswers[index]){
+            score= score + 1;
+        }
+        index =index + 1;
+    }
+  outputEl.innerText ="Your score is " + score;
+ }
+
+submitAnswerBtn.addEventListener("click", calculateScore)
